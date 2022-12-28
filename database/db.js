7df11cca680837f42c386ffa91a01b557fde1316
eNRIQUE~ <quiqecerja@gmail.com>
2022-12-27 23:59:34 -0600

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const conexion = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    pass: process.env.DB_PASS || 'root',
+    pass: process.env.DB_PASS || '',
     database: process.env.DB_DATABASE || 'crud_nods'
 })
 
