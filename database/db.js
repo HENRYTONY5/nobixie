3fceff8df4a2eceb9668f09034060dc8ff48c7eb
eNRIQUE~ <quiqecerja@gmail.com>
2022-12-28 21:11:39 -0600

    ok
    ok3

--- database/db.js
+++ database/db.js
@@ -1,18 +1,21 @@
-const mysql = require('mysql')
+const mysql = require('mysql2/promise')
 
 const conexion = mysql.createConnection({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
-    database : process.env.DB_DATABASE,
+    database : process.env.DB_DATABASE
 })
 
-conexion.connect ((error) => {
-    if(error) {
-        console.error('The connection error is:' + error)
-        return
-    }
-    console.log('Connected to the database MySQL!')
-})
+// pool.connect ((error) => {
+//     if(error) {
+//         console.error('The connection error is:' + error)
+//         return
+//     }
+//     console.log('Connected to the database MySQL!')
+// })
+
 
 module.exports = conexion
+
+
